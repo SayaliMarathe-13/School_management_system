11e6ddd7323b728267adc71c4ac4a66227d536fa
SayaliMarathe-13 <marathesayali2003@gmail.com>
2023-11-08 13:56:11 +0530

    created login signup pages and according to usertype the user should be entered into login section they may be teacher or they may be student

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-var http = require('http');
+const express = require('express');
+const app = express();
+const session = require('express-session');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(3000);
+app.use(express.static('public'));
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.set('view engine', 'ejs');
+
+// Create a route for the dashboard
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard', {
+        title: 'Dashboard',
+        TeachersCount: 67,
+        StudentsCount: 88,
+        ClassesCount: 99,
+        Notifications: 78,
+        Performers: [
+            { Name: 'Emma Johnson', Percentage: '98%', Rank: 'Rank 1' },
+            { Name: 'Sophia Wilson', Percentage: '95%', Rank: 'Rank 2' },
+            // Add more project data here as needed
+        ],
+    });
+});
+
+// Create a route for the login page
+app.get('/login', (req, res) => {
+    const userType = req.query.type; // Get the userType from the query parameter
+    req.session.userType = userType; // Set the userType in the session
+
+    res.render('login', { userType: userType });
+});
+
+app.get('/sign_up', (req, res) => {
+    res.render('sign up');
+});
+
+// Start the Express server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
